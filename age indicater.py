92a80cd67362f6993615ea014adb08e302fc770c
Lokesh Joshi <69955726+lokeshjoshi01@users.noreply.github.com>
2020-08-20 13:33:24 +0530

    Add files via upload

---
+++ age indicater.py
@@ -0,0 +1,9 @@
+print("enter your age")
+age = int(input())
+
+if age>18:
+    print("you can drive")
+elif age==18:
+    print("you can drive but you are under process")
+else:
+    print("you can not drive")
